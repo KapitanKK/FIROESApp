228c14d020e99dd661de326bb1a7fea88302c448
KapitanKK <orgaskiel5@gmail.com>
2023-03-25 17:57:29 +0800

    App Icon Code

--- app/src/main/java/com/example/firoesapp/MainActivity.java
+++ app/src/main/java/com/example/firoesapp/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.baseline_bedtime_24);
         setContentView(R.layout.activity_main);
     }
 }
