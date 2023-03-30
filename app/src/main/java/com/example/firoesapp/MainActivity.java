ec5280ee90daded71d42e0e2f9145617b6ead6df
ThinkYeahxD <94166779+ThinkYeahxD@users.noreply.github.com>
2023-03-30 19:12:24 +0800

    Add files via upload

--- app/src/main/java/com/example/firoesapp/MainActivity.java
+++ app/src/main/java/com/example/firoesapp/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ViewFlipper viewFlipper;
+    Button next1;
+    Button prev1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.baseline_bedtime_24);
         setContentView(R.layout.activity_main);
+
+
+        // buttons in image next or previous
+        viewFlipper = (ViewFlipper) findViewById(R.id.ViewFlipper);
+        next1 = (Button) findViewById(R.id.next1);
+        prev1 = (Button) findViewById(R.id.prev1);
+
+        next1.setOnClickListener(this);
+        prev1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View btn) {
+
+        if (btn == next1){
+            viewFlipper.showNext();
+        } else if (btn == prev1){
+            viewFlipper.showPrevious();
+        }
 
     }
 }
